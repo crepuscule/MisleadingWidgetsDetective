11bb9680f16d3046c8c62ae8a11e86c893cebb93
crepuscule <twilight_wang@163.com>
2020-12-07 19:36:56 +0800

    move to TIANV

--- Console/urls.py
+++ Console/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,re_path
 
 from . import views
 
@@ -8,9 +8,33 @@ app_name='[Console]'
 # 本文件设置本应用的路由
 urlpatterns = [
     path('',views.index,name='index'),
-    path('homepage',views.homepage), 
-    path('console/<str:projectName>/<str:subtask_name>',views.console), 
-    path('gallery/<str:projectName>/<str:subtask_name>/<str:galleryId>',views.gallery), 
-    path('tagging',views.tagging), 
+    path('home',views.homepage), 
+    path('checkRun',views.checkRun), 
+    # Console/console.html
+    path('console',views.console), 
+    # Console/submitRun.html
+    path('submitRun',views.submitRun), 
+    # Console/projects.html
+    path('projects',views.projects), 
+    # Console/gallerys.html
+    path('gallerys',views.gallerys), 
+    #Console/tags.html
+    path('tags',views.tags), 
+    # Console/subtask.html
+    path('subtask/<str:projectName>/<str:subtask_name>',views.subtask), 
+    # Console/album_outlier.html
+    path('album/<str:projectName>/<str:subtask_name>/<str:galleryId>/<str:hightlightClusterId>',views.album), 
+    #re_path(r'^album/(?P<projectName>[0-9a-z]*)/(?P<subtask_name>[0-9a-z]*)/<(?P<galleryId>[0-9a-z]*)/(?P<hightlightCluster>[0-9a-z]*)/(?P<hightlightId>[0-9a-z]*)',views.album), 
+    # Console/album_tagging.html
+    path('tagging/<str:projectName>/<str:subtask_name>/<str:widget_id>',views.tagging), 
+    # Console/evaluate.html
+    path('login',views.login),
+    path('users',views.users),
+    path('addUser',views.addUser),
+    path('deleteUser',views.deleteUser),
+    path('evaluateHome',views.evaluateHome),
+    path('evaluate/<str:projectName>/<str:subtask_name>',views.evaluate), 
+    path('evaluateSubmit',views.evaluateSubmit), 
+    path('changeDB',views.changeDB)
 ]
 
